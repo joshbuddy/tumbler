75e2b2da724b322aa4742822c11ea20520b88a1e
Joshua Hull <joshbuddy@gmail.com>
2010-06-17 19:43:30 -0400

    Cleaned up spec, removed debug output

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,51 +2,50 @@ require 'spec/spec_helper'
 
 describe 'Tumbler#version' do
   it "should read the current version" do
-    create_app('test', :version => '0.1.2') { |tumbler|
-      tumbler.version.to_s.should == '0.1.2'
+    create_app('test', :version => '0.1.2') { |app|
+      app.version.to_s.should == '0.1.2'
     }
   end
 
   it "should bump the current version by minor" do
-    create_app('test', :version => '0.1.2') { |tumbler|
-      puts "File.read(tumbler.version.file) #{File.read(tumbler.version.file)}"
+    create_app('test', :version => '0.1.2') { |app|
       Tumbler::Gem.any_instance.stubs(:push).once.returns(true)
-      tumbler.bump_and_push(:minor)
-      tumbler.version.to_s.should == '0.2.0'
-      tumbler.tags.should include('0.2.0')
+      app.bump_and_push(:minor)
+      app.version.to_s.should == '0.2.0'
+      app.tags.should include('0.2.0')
     }
   end
 
   it "should bump the current version by tiny" do
-    create_app('test', :version => '0.1.2') { |tumbler|
+    create_app('test', :version => '0.1.2') { |app|
       Tumbler::Gem.any_instance.stubs(:push).once.returns(true)
-      tumbler.bump_and_push(:tiny)
-      tumbler.version.to_s.should == '0.1.3'
+      app.bump_and_push(:tiny)
+      app.version.to_s.should == '0.1.3'
     }
   end
 
   it "should bump the current version by major" do
-    create_app('test', :version => '0.1.2') { |tumbler|
+    create_app('test', :version => '0.1.2') { |app|
       Tumbler::Gem.any_instance.stubs(:push).once.returns(true)
-      tumbler.bump_and_push(:major)
-      tumbler.version.to_s.should == '1.0.0'
+      app.bump_and_push(:major)
+      app.version.to_s.should == '1.0.0'
     }
   end
 
   it "should bump the current version by major" do
-    create_app('test', :version => '0.1.2') { |tumbler|
+    create_app('test', :version => '0.1.2') { |app|
       Tumbler::Gem.any_instance.stubs(:push).once.returns(true)
-      tumbler.bump_and_push(:major)
-      tumbler.version.to_s.should == '1.0.0'
+      app.bump_and_push(:major)
+      app.version.to_s.should == '1.0.0'
     }
   end
 
   it "should not let you tag the same version twice" do
-    create_app('test', :version => '0.1.2') { |tumbler|
+    create_app('test', :version => '0.1.2') { |app|
       Tumbler::Gem.any_instance.stubs(:push).once.returns(true)
-      tumbler.bump_and_push(:major)
-      tumbler.version.to_s.should == '1.0.0'
-      proc {tumbler.tag_and_push(:major)}.should raise_error
+      app.bump_and_push(:major)
+      app.version.to_s.should == '1.0.0'
+      proc {app.tag_and_push(:major)}.should raise_error
       
     }
   end
