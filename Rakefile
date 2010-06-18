bdb7ed7555776b1cb842a12b973945cafce51fa1
Joshua Hull <joshbuddy@gmail.com>
2010-06-17 22:39:59 -0400

    Added preflight task for easy injection of arbitrary code before tumbler runs.

--- Rakefile
+++ Rakefile
@@ -2,6 +2,10 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
 require 'tumbler'
 Tumbler.use_rake_tasks
 
+task 'tumbler:preflight' do
+  Rake::Task["spec"].invoke
+end
+
 require 'spec'
 require 'spec/rake/spectask'
 Spec::Rake::SpecTask.new(:spec) do |t|
--- lib/tumbler/rake_tasks.rb
+++ lib/tumbler/rake_tasks.rb
@@ -16,47 +16,52 @@ module Tumbler
 
     def activate_project_tasks(protect_namespace)
       tasks = proc do
+        namespace :tumbler do
+          task :preflight do
+          end
+        end
+
         namespace :gem do
           desc "Build the gem"
-          task :build do
+          task :build => 'tumbler:preflight' do
             @manager.gem.build
           end
 
           desc "Push the gem"
-          task :push do
+          task :push => 'tumbler:preflight' do
             @manager.gem.push
           end
 
           desc "Install the gem"
-          task :install do
+          task :install => 'tumbler:preflight' do
             @manager.gem.install
           end
         end
 
         namespace :version do
           desc "Tag current version into git"
-          task :tag do
+          task :tag => 'tumbler:preflight' do
             @manager.tag
           end
         
           desc "Push current version into git"
-          task :push do
+          task :push => 'tumbler:preflight' do
             @manager.tag_and_push
           end
         
           @manager.version.field_names.each do |field|
             namespace field do 
               desc "Bump version from #{@manager.version.to_s} ->  #{@manager.version.value.bump(field).to_s}"
-              task :bump do
+              task :bump => 'tumbler:preflight' do
                 @manager.bump_and_commit(field)
               end
 
-              task :push do
+              task :push => 'tumbler:preflight' do
                 @manager.tag_and_push(field)
               end
 
               desc "Bump version from #{@manager.version.to_s} ->  #{@manager.version.value.bump(field).to_s} and push"
-              task :release do
+              task :release => 'tumbler:preflight' do
                 @manager.bump_and_push(field)
               end
             end
