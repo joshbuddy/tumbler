016ee167ef408e1aa23b276e5e1c78f781ad8191
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-19 13:45:33 -0700

    merged in bin_tumbler tests into cli_test

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -1,7 +1,10 @@
 require File.join(File.dirname(__FILE__),'teststrap')
 
 context "Cli" do
-  setup { `rm -rf /tmp/test/icles` }
+  setup { @test_dir = temp_dir('test') }
+  setup { FileUtils.rm_rf @test_dir }
+  teardown { FileUtils.rm_rf @test_dir }
+
   context "shows the help" do
     setup { capture(:stdout) { Tumbler::Cli.start(['-h']) } }
     asserts_topic.matches  %r{tumbler name \[options\]}
@@ -11,10 +14,55 @@ context "Cli" do
     asserts_topic.matches  %r{set root path}
   end
 
-  context "generates at designated root" do
-    setup { capture(:stdout) { Tumbler::Cli.start(['icles', '-r=/tmp/test/']) } }
-    asserts_topic.matches %r{icles successfully generated!}
-    asserts("in the right path") { File.exists?('/tmp/test/icles/') }
+  context "generates" do
+    context "the app" do
+      setup do
+        capture(:stdout) { Tumbler::Cli.start(['test_gem',"-r=#{@test_dir}"]) }
+        Tumbler.load(File.join(@test_dir, 'test_gem'))
+      end
+      asserts("$?") { $? }.equals 0
+      asserts("version") { topic.version.to_s }.equals '0.0.0'
+      asserts("tumbler") { topic.bundler.dependencies.first.name }.equals 'tumbler'
+      asserts(">=0") { topic.bundler.dependencies.first.requirements_list }.equals ['>= 0']
+      asserts("development") { topic.bundler.dependencies.first.groups }.equals [:development]
+    end
+
+    context "at designated root" do
+      setup { capture(:stdout) { Tumbler::Cli.start(['agem', "-r=#{@test_dir}"]) } }
+      asserts_topic.matches %r{agem successfully generated!}
+      asserts("in the right path") { File.exists? File.join(@test_dir,'agem') }
+    end
+
   end
+
+  context "upgrades" do
+    setup { capture(:stdout) { Tumbler::Cli.start(['rails', "-r=#{@test_dir}"]) } }
+
+    context "does nothing on a normal existing tumbler app" do
+      setup do
+        @tumbler = Tumbler.load(File.join(@test_dir, 'rails'))
+        @version = File.read @tumbler.version.file
+        @changelog = File.read @tumbler.changelog.file
+        @gemfile = File.read @tumbler.gemfile_path
+        @gemspec = File.read @tumbler.gemspec_path
+      end
+      setup { capture(:stdout) { Tumbler::Cli.start(['rails', '-u', "-r=#{@test_dir}"]) } }
+      asserts("$?") { $? }.equals 0
+      asserts("version") { File.read(@tumbler.version.file) == @version }
+      asserts("changelog") { File.read(@tumbler.changelog.file) == @changelog }
+      asserts("gemfile") { File.read(@tumbler.gemfile_path) == @gemfile }
+      asserts("gemspec") { File.read(@tumbler.gemspec_path) == @gemspec }
+    end
+
+    context "should not work if the directory and gemspec mismatch and you don't supply a name" do
+      setup do
+        FileUtils.mv("#{@test_dir}/rails/rails.gemspec", "#{@test_dir}/rails/notrails.gemspec")
+      end
+      asserts "exit status" do 
+        capture(:stdout) { Tumbler::Cli.start(['rails', '-u',"-r=#{@test_dir}"]) }
+      end.raises RuntimeError
+    end
+  end
+
 
 end
