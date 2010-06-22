1cec0cddb28a504ae6a0997bfbdd0826ceb8aacf
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-21 17:29:03 -0700

    added test for Tumbler

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -42,11 +42,21 @@ context "Generator" do
       asserts("require") { File.read topic }.matches %r{source :rubygems}
       asserts("rake tasks") { File.read topic }.matches %r{gem "tumbler"}
     end
+
+    context "Tumbler" do
+      setup { File.join(@test_dir, 'my_gem', 'Tumbler') }
+      asserts("gem_name") { File.read topic }.matches %r{gem_name my_gem}
+      asserts("version_file") { File.read topic }.matches %r{version_file 'lib/my_gem/version.rb'}
+      asserts("changelog") { File.read topic }.matches %r{changelog_file CHANGELOG}
+    end
+
   end
 
   context "suppress changelog creation if disabled" do
     setup { capture(:stdout) { Tumbler::Cli.start(['new_gem','-c=none', "-r=#{@test_dir}"]) } }
+    setup { File.join(@test_dir, 'new_gem', 'Tumbler') }
     asserts("has changelog") { File.exist? File.join(@test_dir,'new_gem', 'CHANGELOG') }.not!
+    asserts("changelog") { File.read(topic) =~ %r{changelog_file CHANGELOG} }.not!
   end
 
   context "generate changelog with specified name" do
@@ -57,12 +67,14 @@ context "Generator" do
   context "surpress version.rb creation if disabled" do
     setup do
       capture(:stdout) { Tumbler::Cli.start(['my_gem','-v=none',"-r=#{@test_dir}"]) }
+      @tumbler = File.join(@test_dir, 'my_gem', 'Tumbler')
       File.join(@test_dir, 'my_gem','lib', 'my_gem.rb')
     end
     asserts("exists") { File.exists? topic }
     asserts("module") { File.read topic }.matches %r{module MyGem #:nodoc}
     asserts("version.rb") { File.exists? File.join(@test_dir, 'my_gem','lib', 'my_gem','version.rb') }.not!
     asserts("require") { File.read(topic) =~ %r{require 'my_gem/version'\n} }.not!
+    asserts("version_file") { File.read(@tumbler) =~ %r{version_file 'lib/my_gem/version.rb'} }.not!
   end
 
   context "generate the version.rb if specified" do
@@ -88,7 +100,7 @@ context "Generator" do
   end
 
   context "generate dependencies" do
-    
+
     context "extra dependency" do
       setup do
         capture(:stdout) { Tumbler::Cli.start(['my_gem','-d=eventmachine,hpricot',"-r=#{@test_dir}"]) }
@@ -97,7 +109,7 @@ context "Generator" do
       asserts("gem 'eventmachine") { File.read topic }.matches %r{gem "eventmachine"}
       asserts("gem 'hpricot") { File.read topic }.matches %r{gem "hpricot"}
     end
-    
+
     context "extra dependency version" do
       setup do
         capture(:stdout) { Tumbler::Cli.start(['my_gem',"-d='eventmachine>=1.0.0'","-r=#{@test_dir}"]) }
