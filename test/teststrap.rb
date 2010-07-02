e0056fa4d005fd149c0a2ad6878ae5e265b925c1
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-07-02 07:51:49 -0700

    change test helpers. change riot reporting to VerboseStoryReporter

--- test/teststrap.rb
+++ test/teststrap.rb
@@ -3,6 +3,7 @@ require 'tempfile'
 require 'fileutils'
 require 'riot'
 require 'mocha'
+require 'git'
 
 $LOAD_PATH << File.basename(__FILE__)
 $LOAD_PATH << File.join(File.basename(__FILE__), '..', 'lib')
@@ -11,22 +12,31 @@ require 'tumbler'
 Tumbler::Gem.any_instance.stubs(:install).raises
 Tumbler::Gem.any_instance.stubs(:push).raises
 
-
-Riot.reporter = Riot::StoryReporter
+Riot.reporter = Riot::VerboseStoryReporter
 class Riot::Situation
 
-  def create_app(name='test', opts={})
-    @dir = temp_dir(name)
-    @remote_dir = temp_dir("remote-#{name}.git")
-    Tumbler::Generate.app(@dir, name, opts).write
-    Dir.chdir(@remote_dir) { `git --bare init` }
-    remote = opts[:remote] || "file://#{@remote_dir}"
-    Dir.chdir(@dir) {
-      `git remote add origin #{remote}`
-      `git push origin master`
-    }
-    [Tumbler::Manager.new("#{@dir}/#{name}.gemspec"), @dir, @remote_dir]
+  def create_app(name,*options)
+    local = "/tmp/#{name}"
+    remote = "/tmp/#{name}/remote-#{name}.git"
+    Tumbler::Cli.start([name,"-r=/tmp", *options])
+    git = ::Git.init("/tmp/test")
+    git.add_remote('origin',remote)
+    #git.push(git.remote('origin'))
+    [Tumbler::Manager.new("/tmp/#{name}/#{name}.gemspec"), local, remote]
   end
+
+  # def create_app(name='test', opts={})
+  #   @dir = temp_dir(name)
+  #   @remote_dir = temp_dir("remote-#{name}.git")
+  #   Tumbler::Generate.app(@dir, name, opts).write
+  #   Dir.chdir(@remote_dir) { `git --bare init` }
+  #   remote = opts[:remote] || "file://#{@remote_dir}"
+  #   Dir.chdir(@dir) {
+  #     `git remote add origin #{remote}`
+  #     `git push origin master`
+  #   }
+  #   [Tumbler::Manager.new("#{@dir}/#{name}.gemspec"), @dir, @remote_dir]
+  # end
 
   def create_bare_app(name, opts = {})
     dir = temp_dir(name)
@@ -35,7 +45,7 @@ class Riot::Situation
     [dir, tumbler]
   end
 
-  def temp_dir(name)
+  def temp_dir(name='test')
     dir = File.join(Dir.tmpdir, rand(10000).to_s, "#{Process.pid}-#{name}")
     FileUtils.rm_rf(dir)
     FileUtils.mkdir_p(dir)
