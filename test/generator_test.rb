1f9a22f22289166788c175c25ac8f85f3633ab19
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-07-11 21:51:14 -0700

    added rspec generation

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -171,6 +171,30 @@ context "Generator" do
       end
     end
 
+    context "rspec" do
+      setup do
+        capture(:stdout) { Tumbler::Cli.start(['my_gem','--test=rspec',"-r=#{@test_dir}"]) }
+        @helper = File.join(@test_dir, 'my_gem', 'test', 'helper.rb')
+        @gem_test =  File.join(@test_dir, 'my_gem', 'test','my_gem_test.rb')
+      end
+      asserts("helper.rb") { File.exist? @helper }
+      asserts("my_gem_test.rb") { File.exist? @gem_test }
+            
+      context "teststrap" do
+        setup { File.read(@helper) }
+        asserts_topic.matches %r{require 'spec'}
+        asserts_topic.matches %r{require 'spec/autorun'}
+        asserts_topic.matches %r{lib/my_gem.rb}
+        asserts_topic.matches %r{Spec::Runner.configure}
+      end
+
+      context "my_gem_test.rb" do
+        setup { File.read(@gem_test) }
+        asserts_topic.matches %r{describe "MyGem"}
+        asserts_topic.matches %r{fails}
+      end
+    end
+
   end
 
 end
